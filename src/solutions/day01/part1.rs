ce51f27224bd86c5dddb0cb1044fe8d3ed8c0904
Giacomo Miolo <giacomo.miolo@gmail.com>
2024-12-01 16:25:34 +0000

    improve day 1 part 1 benches with preallocation and avoiding intermediate collection

--- src/solutions/day01/part1.rs
+++ src/solutions/day01/part1.rs
@@ -6,29 +6,26 @@ pub fn solve() -> Result<i64> {
 }
 
 pub fn solve_for_input(input: &str) -> Result<i64> {
-    let mut left_numbers = Vec::new();
-    let mut right_numbers = Vec::new();
+    let line_count = input.lines().count();
+    let mut left_numbers = Vec::with_capacity(line_count);
+    let mut right_numbers = Vec::with_capacity(line_count);
 
     for line in input.lines() {
-        let numbers: Vec<i64> = line
+        let mut iter = line
             .split_whitespace()
-            .filter_map(|s| s.parse().ok())
-            .collect();
-
-        if numbers.len() == 2 {
-            left_numbers.push(numbers[0]);
-            right_numbers.push(numbers[1]);
+            .filter_map(|s| s.parse::<i64>().ok());
+        
+        if let (Some(left), Some(right)) = (iter.next(), iter.next()) {
+            left_numbers.push(left);
+            right_numbers.push(right);
         }
     }
 
     left_numbers.sort_unstable();
     right_numbers.sort_unstable();
 
-    let total_distance: i64 = left_numbers
+    Ok(left_numbers
         .iter()
         .zip(right_numbers.iter())
-        .map(|(a, b)| (a - b).abs())
-        .sum();
-
-    Ok(total_distance)
+        .fold(0, |acc, (a, b)| acc + (a - b).abs()))
 }
